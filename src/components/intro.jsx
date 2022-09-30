6f1a649a25d033df1826f8a2f4e2d76d400db153
Praisecodes <cmancoder@gmail.com>
2022-09-30 17:22:49 +0100

    [Update] Intro section

---
+++ src/components/intro.jsx
@@ -0,0 +1,21 @@
+import {FaFacebookF, FaTwitter, FaLinkedin, FaInstagramSquare} from "react-icons/fa"
+
+export default function Intro(){
+    return (
+        <div className="intro">
+            <div className="writeUp">
+                <div className="socials">
+                    <a href="https://facebook.com/PraiseCodes"><i><FaFacebookF /></i></a>
+                    <a href="https://twitter.com/PraiseCodes"><i><FaTwitter /></i></a>
+                    <a href="https://linkedin.com"><i><FaLinkedin /></i></a>
+                    <a href="https://instagram.com"><i><FaInstagramSquare /></i></a>
+                </div> <br /><br />
+                <h1>I am Okeypraise Junior</h1><br />
+                <p>I am Okeypraise Junior<br />
+                A fullstack web developer with 2 years experience as a software developer.</p> <br />
+                <button>Hire Me</button>
+            </div>
+            <div className="image"></div>
+        </div>
+    )
+}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,11 @@
 import Header from "../components/header"
+import Intro from "../components/intro"
 
 export default function Home(){
     return (
+        <>
         <Header />
+        <Intro />
+        </>
     )
 }
