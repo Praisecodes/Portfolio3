01b5995a6932198647722ce03edd0ee5574eb97b
Praisecodes <cmancoder@gmail.com>
2022-09-30 22:46:58 +0100

    [Update] Added links

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -6,10 +6,10 @@ export default function Intro(){
         <div className="intro">
             <div className="writeUp">
                 <div className="socials">
-                    <a href="https://facebook.com/PraiseCodes"><i><FaFacebookF /></i></a>
+                    <a href="https://web.facebook.com/praisecodes"><i><FaFacebookF /></i></a>
                     <a href="https://twitter.com/PraiseCodes"><i><FaTwitter /></i></a>
-                    <a href="https://linkedin.com"><i><FaLinkedin /></i></a>
-                    <a href="https://instagram.com"><i><FaInstagramSquare /></i></a>
+                    <a href="hhttps://www.linkedin.com/in/okechukwu-praise-okeypraise-97a961239/"><i><FaLinkedin /></i></a>
+                    <a href="https://www.instagram.com/praisecodes/"><i><FaInstagramSquare /></i></a>
                 </div> <br /><br />
                 <h1>I am Okeypraise Junior</h1><br />
                 <p>I am Okeypraise Junior<br />
