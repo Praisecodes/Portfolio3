4e57f12ebd833babedee630a381f2dfefb448aba
Praisecodes <cmancoder@gmail.com>
2022-09-30 16:21:14 +0100

    Commit initial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/home'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/components/header.jsx
@@ -0,0 +1,19 @@
+export default function Header(){
+    return (
+        <>
+        <div className="header">
+            <h1>
+                PraiseC<span>o</span>des
+            </h1>
+            <ul>
+                <li>Home</li>
+                <li>Services</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Blog</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+        </>
+    )
+}
