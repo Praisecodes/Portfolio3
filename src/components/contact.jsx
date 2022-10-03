eb558934d1f234a25a024fcda656c37b2f7e6671
Praisecodes <cmancoder@gmail.com>
2022-10-03 14:24:44 +0100

    [Update] Enabling send email feature

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,13 +1,31 @@
+import { useState } from "react"
+
 export default function Contact(){
+    const [message, setMessage] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [sending, setSending] = useState(false);
+
+    function sendMessage(e){
+        e.preventDefault();
+
+        fetch(`https://portfoliopraise.000webhostapp.com/sendemail.php?message=${message}&&name=${name}&&email=${email}`)
+        .then(res=>res.json())
+        .then((data)=>{console.log(data); setSending(false)})
+        .catch((err)=>{console.log(err); setSending(false)});
+    }
+
     return (
         <div className="contact">
             <h1>Contact Me</h1><br /><br />
 
-            <form onSubmit={(e)=>{e.preventDefault()}}>
-                <input type="text" placeholder="Full Name" />
-                <input type="email" placeholder="Email" />
-                <textarea name="text" placeholder="Message" ></textarea>
-                <button type="submit">Send</button>
+            <form onSubmit={(e)=>{sendMessage(e); setSending(true)}}>
+                <input type="text" placeholder="Full Name" onChange={(e)=>{setName(e.target.value)}} />
+                <input type="email" placeholder="Email" onChange={(e)=>{setEmail(e.target.value)}} />
+                <textarea name="text" placeholder="Message" onChange={(e)=>{setMessage(e.target.value)}} ></textarea>
+                <button type="submit">
+                    {(sending)?"Sending...":"Send"}
+                </button>
             </form>
         </div>
     )
